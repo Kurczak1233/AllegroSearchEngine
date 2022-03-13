36be620a156c67c170e3bacdf551e32d50fb5f96
holygwent <39002523+holygwent@users.noreply.github.com>
2022-03-13 22:38:26 +0100

    Adding MongoDB connection and Service

---
+++ Server/Server/AllLook.Database/AllLookDatabaseSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllLook.Database
+{
+    public class AllLookDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string ProductsCollectionName { get; set; }
+    }
+}
---
+++ Server/Server/AllLook.Database/IDatabaseProductService.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllLook.Database
+{
+    public interface IDatabaseProductService
+    {
+      public  List<Product> GetProductsCollection();
+    }
+}
